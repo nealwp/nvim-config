92490527813d8998d8ada83fade481f3c5a9df1f
nealwp <mrprestonneal@gmail.com>
2023-05-06 13:17:31 -0700

    add setup script and bootstrap for packer

--- lua/nealwp/packer.lua
+++ lua/nealwp/packer.lua
@@ -1,11 +1,25 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_boostrap = ensure_packer()
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
+
 
 	use 'Mofiqul/vscode.nvim'
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
@@ -14,20 +28,25 @@ return require('packer').startup(function(use)
 		'VonHeikemen/lsp-zero.nvim',
   		branch = 'v2.x',
   		requires = {
-    		-- LSP Support
-    		{'neovim/nvim-lspconfig'},             -- Required
-    		{                                      -- Optional
-      			'williamboman/mason.nvim',
-      			run = function()
-        			pcall(vim.cmd, 'MasonUpdate')
-      			end,
-    		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    			-- LSP Support
+    			{'neovim/nvim-lspconfig'},             -- Required
+    			{                                      -- Optional
+      				'williamboman/mason.nvim',
+      				run = function()
+        				pcall(vim.cmd, 'MasonUpdate')
+      				end,
+    			},
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-    		-- Autocompletion
-    		{'hrsh7th/nvim-cmp'},     -- Required
-    		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-    		{'L3MON4D3/LuaSnip'},     -- Required
-  	}
-}
+    			-- Autocompletion
+    			{'hrsh7th/nvim-cmp'},     -- Required
+    			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+    			{'L3MON4D3/LuaSnip'},     -- Required
+  		}
+	
+	}
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
