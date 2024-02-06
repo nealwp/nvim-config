8174a4196384b0c705643fb34634000802b5c899
Preston Neal <mrprestonneal@gmail.com>
2024-02-06 15:06:33 -0800

    add live grep keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,11 +7,16 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 -- ctrl + p to search files in git
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
--- leader g to grep a string
-vim.keymap.set('n', '<leader>g', function()
+-- leader s to search a word
+vim.keymap.set('n', '<leader>s', function()
     builtin.grep_string({ search = vim.call("expand", "<cword>") });
 end)
 
+-- leader g to live grep 
+vim.keymap.set('n', '<leader>g', function()
+    builtin.live_grep();
+end)
+--
 -- leader r to show references
 vim.keymap.set('n', 'gr', builtin.lsp_references, {})
 --]]
