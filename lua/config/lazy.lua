5672ee9800f6d4bf16698bbedf76252abd556fcd
Preston Neal <mrprestonneal@gmail.com>
2024-12-28 20:31:26 -0800

    fix telescope keymaps

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -87,3 +87,17 @@ require("lazy").setup({
 })
 
 vim.cmd.colorscheme "catppuccin"
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ex', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>s', function()
+    builtin.grep_string({ search = vim.call('expand', '<cword>') });
+end)
+
+vim.keymap.set('n', '<leader>g', function()
+    builtin.live_grep();
+end)
