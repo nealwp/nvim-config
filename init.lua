78a150a17272c5db3e76f965f05679e9992e5894
Preston Neal <mrprestonneal@gmail.com>
2024-12-31 16:33:06 -0800

    move telescope keymaps

--- init.lua
+++ init.lua
@@ -85,20 +85,3 @@ vim.keymap.set("v", "<", "<gv")
 
 -- replace word under cursor with yanked text
 vim.keymap.set("n", "S", "\"_diwP")
-
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '_', builtin.find_files)
-vim.keymap.set('n', '<leader>h', builtin.help_tags)
-vim.keymap.set('n', '<leader>k', builtin.keymaps)
-vim.keymap.set('n', '<leader>q', builtin.quickfix)
-vim.keymap.set('n', '<leader>b', builtin.buffers)
-vim.keymap.set('n', '<leader>f', builtin.git_files)
-
-vim.keymap.set('n', '<leader>s', function()
-    builtin.grep_string({ search = vim.call('expand', '<cword>') });
-end)
-
-vim.keymap.set('n', '<leader>g', function()
-    builtin.live_grep();
-end)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,4 +4,30 @@ return {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
+    config = function()
+        require('telescope').setup({
+            extensions = {
+                fzf = {}
+            }
+        })
+
+        require('telescope').load_extension('fzf')
+
+        local builtin = require('telescope.builtin')
+
+        vim.keymap.set('n', '_', builtin.find_files)
+        vim.keymap.set('n', '<leader>h', builtin.help_tags)
+        vim.keymap.set('n', '<leader>k', builtin.keymaps)
+        vim.keymap.set('n', '<leader>q', builtin.quickfix)
+        vim.keymap.set('n', '<leader>b', builtin.buffers)
+        vim.keymap.set('n', '<leader>f', builtin.git_files)
+
+        vim.keymap.set('n', '<leader>s', function()
+            builtin.grep_string({ search = vim.call('expand', '<cword>') });
+        end)
+
+        vim.keymap.set('n', '<leader>g', function()
+            builtin.live_grep();
+        end)
+    end
 }
