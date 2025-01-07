52fae5eea489a2d7c1b4589f4cf79a6d0c152736
Preston Neal <mrprestonneal@gmail.com>
2025-01-07 13:51:28 -0800

    fix cmdline completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -11,7 +11,7 @@ return {
             ['<C-j>'] = { 'select_next', 'fallback' },
 
             -- `Enter` key to confirm completion
-            ['<Enter>'] = { 'accept', 'fallback' },
+            ['<CR>'] = { 'select_and_accept', 'fallback' },
 
             -- Ctrl+Space to trigger completion menu
             ['<C-space>'] = { 'show', 'fallback' },
@@ -19,11 +19,19 @@ return {
             -- Scroll up and down in the completion documentation
             ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
             ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+
+            cmdline = {
+                preset = 'none',
+
+                ['<C-k>'] = { 'select_prev', 'fallback' },
+                ['<C-j>'] = { 'select_next', 'fallback' },
+                ['<Tab>'] = { 'select_and_accept', 'fallback' },
+            }
         },
 
         appearance = {
             use_nvim_cmp_as_default = false,
-            nerd_font_variant = 'mono'
+            nerd_font_variant = 'normal'
         },
         completion = {
             documentation = { auto_show = true, auto_show_delay_ms = 0 },
