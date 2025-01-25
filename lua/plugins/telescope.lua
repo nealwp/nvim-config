c5973bebe1fc7a65c3a1ee6554acab78c85ce2d2
Preston Neal <mrprestonneal@gmail.com>
2025-01-25 10:18:04 -0800

    only load fzf on not windows

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,13 +5,19 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
-        require('telescope').setup({
-            extensions = {
-                fzf = {}
-            }
-        })
+        local is_windows = jit.os == "Windows"
 
-        require('telescope').load_extension('fzf')
+        if not is_windows then
+            require('telescope').setup({
+                extensions = {
+                    fzf = {}
+                }
+            })
+
+            require('telescope').load_extension('fzf')
+        else
+            require('telescope').setup()
+        end
 
         local builtin = require('telescope.builtin')
 
