7b01911fd828e555e6a1ce46bdc7ba7ea332cd8a
Preston Neal <mrprestonneal@gmail.com>
2025-02-10 09:17:18 -0800

    formatter

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,25 +1,26 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				python = { "black", "ruff", stop_after_first = true },
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-			},
-		})
-	end,
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                css = { "prettier" },
+                scss = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                python = { "black", "ruff", stop_after_first = true },
+                lua = { "stylua" },
+                sql = { "sqlfmt" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+            },
+        })
+    end,
 }
